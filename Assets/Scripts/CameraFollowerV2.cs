529f8c8ed78d24ed466583399ec43878ee8d6258
RivadonS <rivadon.sung@bumail.net>
2026-01-30 16:15:24 +0700

    Camera Finish

--- Assets/Scripts/CameraFollowerV2.cs
+++ Assets/Scripts/CameraFollowerV2.cs
@@ -12,11 +12,43 @@ public class CameraFollowerV2 : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset;
+    public Vector3 rearView;
+    public Vector3 frontView;
+    public Vector3 FrontView2;
+    private int currentView = 0;
+
+    void Start()
+    {
+        offset = rearView;
+    }
 
     void Update()
     {
         // Student code starts here ...
         // handle input and switch camera view
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            {
+                currentView++;
+            }
+
+            switch(currentView)
+            {
+                case 1:
+                    offset = frontView;
+                    break;
+
+                case 2:
+                    offset = FrontView2;
+                    break;
+
+                default:
+                    offset = rearView;
+                    currentView = 0;
+                    break;
+            }
+            
+        }
     }
 
     void LateUpdate()
