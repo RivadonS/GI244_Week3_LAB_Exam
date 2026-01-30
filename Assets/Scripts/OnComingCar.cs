603a6b56fc6ae9ea18a73ff601d69d4d1696f49f
RivadonS <rivadon.sung@bumail.net>
2026-01-30 15:35:24 +0700

    OnComingCar Finish

--- Assets/Scripts/OnComingCar.cs
+++ Assets/Scripts/OnComingCar.cs
@@ -7,15 +7,5 @@ public class OnComingCar : MonoBehaviour
 {
     public float speed = 10.0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    void Update() => transform.Translate(Vector3.forward * speed * Time.deltaTime);
 }
